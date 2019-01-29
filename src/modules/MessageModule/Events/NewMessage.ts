234d89ec950eca8158410fc1a2c9111e7e9d6461
Ian Wijma <ian@wij.ma>
2019-01-29 23:07:59 +0100

    Added TS Node Starter

--- src/modules/MessageModule/Events/NewMessage.ts
+++
@@ -1,5 +0,0 @@
-import {AbstractModuleEvent} from "../../../abstracts/AbstractModuleEvent";
-
-export default class NewMessage extends AbstractModuleEvent {
-    eventName: string = 'newMessage';
-}
--- src/modules/MessageModule/Models/MessageModel.ts
+++
@@ -1,7 +0,0 @@
-import DatabaseManager from "../../../core/DatabaseManager";
-import * as Bookshelf from "bookshelf";
-let bookshelf:Bookshelf = DatabaseManager.ormDb();
-
-export default class MessageModel extends bookshelf.Model<MessageModel> {
-    tableName:string = 'message';
-}
